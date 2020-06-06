57468b7c77cd65ba615cb8024b492a1bb2c5198b
Samir Chadha <imasheep1386@users.noreply.github.com>
2020-06-06 12:11:34 +0100

    updated for prose poems and python3

--- scrape.py
+++ scrape.py
@@ -9,22 +9,25 @@ Eric Li
 
 from __future__ import print_function
 from bs4 import BeautifulSoup
-import urllib2
+import requests
 import re
-import HTMLParser
+from html.parser import HTMLParser
 
-poet = raw_input('Enter a poet: ')
+poet = input('Enter a poet: ')
 
 poet = poet.lower()
-poet = re.sub('[^a-z]+','-',poet)
+poetx = re.sub('[^a-z]+','-',poet)
 
-fileout = poet + ".txt"
-output = open(fileout,'w')
 
-url = "http://www.poetryfoundation.org/bio/"+poet+"#about"
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
-parser = HTMLParser.HTMLParser()
+
+#fileout = poet + ".txt"
+#output = open(fileout,'w')
+
+
+url = "http://www.poetryfoundation.org/bio/"+poetx+"#about"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, features="html5lib")
+parser = HTMLParser()
 
 poems = soup.find_all('a',href=re.compile('.*/poems/[0-9]+/.*'))
 poems2 = soup.find_all('a',href=re.compile('.*/poem/.*'))
@@ -32,10 +35,32 @@ poems2 = soup.find_all('a',href=re.compile('.*/poem/.*'))
 poems.extend(poems2)
 
 for poem in poems:
+		poemURL = poem.get('href')
+		poemPage = requests.get(poemURL)
+		poemSoup = BeautifulSoup(poemPage.text, features="html5lib")
+		poemTitle = poemSoup.find('h1')
+		
+		if poemTitle:
+			print(parser.unescape(poemTitle.text))
+			print(poet.title())
+			poemContent = poemSoup.find('div', {'class' : 'o-poem'})
+			poemLines = poemContent.findAll('div')
+			poemBlocks = poemContent.findAll('p')
+			for line in poemLines:
+				
+				text = parser.unescape(line.text)
+				print(str(text))
+			for line in poemBlocks:
+				para = parser.unescape(line.text)
+				print(str(para)) 
+				
+
+"""
+for poem in poems:
 
     poemURL = poem.get('href')
-    poemPage = urllib2.urlopen(poemURL)
-    poemSoup = BeautifulSoup(poemPage.read())
+    poemPage = requests.get(poemURL)
+    poemSoup = BeautifulSoup(poemPage.text, features="html5lib")
     
     poemTitle = poemSoup.find('h1')
     
@@ -48,4 +73,5 @@ for poem in poems:
             text = parser.unescape(line.text)
             out = text.encode('utf8')
             print(out,file=output)
-        
+      
+        """  
